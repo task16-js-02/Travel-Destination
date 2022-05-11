9413e127cc806516d142fbb31bc30e02f550a9b0
Muradazzeh <morad.azza@yahoo.com>
2022-05-11 16:20:28 -0700

    updated sol

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,32 +1,17 @@
-import Header from "../header/Header.js";
+import Header from "../header/Header";
 import Footer from "../footer/Footer.js";
-<<<<<<< HEAD
 import Countrys from "../tours/Tours.js";
 const countryData = require("../../data/db.json");
-=======
-import tourData from "../../data/db.json";
-// const tourData = require("./data/db.json");
->>>>>>> class-16murad
 
 function Home(props) {
   return (
     <>
       <h1></h1>
       <Header />
-<<<<<<< HEAD
       {countryData.map((country) => {
         return <Countrys name={country.name} images={country.image} alt={country.id} />;
       })}
       
-=======
-{
- tourData.map(tour=>{
-     return (
-         <Tours name={tour.name}  image={tour.image}/>
-     )
- })
-}
->>>>>>> class-16murad
       <Footer />
     </>
   );
