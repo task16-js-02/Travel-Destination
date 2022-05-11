ded354ea3b977b9af012158983cb4ba7ecb0fa05
Muradazzeh <morad.azza@yahoo.com>
2022-05-10 21:52:13 -0700

    footer update

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,13 @@
 import Footer from "../footer/Footer";
 
+import Header from "../header/Header";
 function Home (props){
     return(
         <>
+        <Header/>
 <h1>Here were we start againe</h1>
 <Footer/>
+
 </>
     )
 
