f278468fef973889120fb47dc0c01c6a0b22f669
ahmadijmail <ahmadijmil2010@gmail.com>
2022-05-11 11:25:12 +0300

    fix3

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/home/Home.js";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
