d7abad833ac3530db002e4917445c4f72aeaea26
ahmadijmail <ahmadijmil2010@gmail.com>
2022-05-11 17:26:25 +0300

    get

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/home/Home.js";
+//import { Routes, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
     <>
       <Home />
+      <h1>Welcome to React Router!</h1>
+
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes> */}
     </>
   );
 }
