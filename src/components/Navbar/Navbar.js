5168d3ae98d175235c9cc4d85d6fd68d392d317f
ahmadijmail <ahmadijmil2010@gmail.com>
2022-05-13 16:30:29 +0300

    bootstrap

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav'
 
 function NavBar() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/city/:id" element={<Header />} />
-      </Routes>
-
-     
+      {/* <Link to="">Home</Link> */}
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </>
   );
 }
